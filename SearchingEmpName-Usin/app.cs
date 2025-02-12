db86d935d4f226f3c76889a77fbbce874f66cfd5
Gowtham <gowthamm0708@gmail.com>
2025-02-12 10:10:15 +0530

    Create app.cs

---
+++ SearchingEmpName-Usin/app.cs
@@ -0,0 +1,22 @@
+.App {
+  margin: 50px;
+}
+
+.search{
+  padding: 10px;
+  font-size: 20px;
+}
+
+.list {
+padding :0;
+list-style:none;
+text-align: center
+}
+
+.employee-list {
+  margin-bottom: 20px;
+  font-size:25px;
+  color-scheme: rgb(183, 185, 158);
+  font-weight: 300;
+}
+
