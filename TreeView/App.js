c9c13f00948e6cf8380b777bbc8a38c5a20c2af0
Gowtham <gowthamm0708@gmail.com>
2025-02-28 11:58:16 +0530

    Create App.js

---
+++ TreeView/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import MultiTreeView from './MultiTreeView'
+function App() {
+  return (
+    <div >
+    <MultiTreeView/>
+    </div>
+  );
+}
+
+export default App;
